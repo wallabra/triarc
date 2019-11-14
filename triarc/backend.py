2a63fae710d7f236c7e75af57fb83a1bbc7d0826
Gustavo Ramos Rehermann <rehermann6046@gmail.com>
2019-11-14 13:31:12 -0300

    Progress with the IRC backend

--- triarc/backend.py
+++ triarc/backend.py
@@ -16,12 +16,5 @@ class Backend(object):
     async def receive_message(self, kind, data):
         if kind in self._listeners:
             async with trio.open_nursery() as nursery:
-                async def call_sync(f):
-                    f(data)
-
                 for listener in self._listeners[kind]:
-                    if inspect.isasyncgenfunction(a):
-                        nursery.start_soon(listener, data)
-
-                    else:
-                        nursery.start_soon(call_sync, listener)
+                    nursery.start_soon(listener, data)
--- triarc/irc.py
+++ triarc/irc.py
@@ -1,14 +1,89 @@
 import trio
+import inspect
+import re
+import queue
 import ssl as pyssl
 
 from triarc.backend import Backend
+from collections import namedtuple
 
+
+
+IRC_SOFT_NEWLINE = re.compile(r'\r?\n')
+
+IRC_RESP_PREFIX = r':([^ \0]+) '
+IRC_RESP_NUMERIC = r'([0-9]{3})'
+IRC_RESP_COMMAND = r'([^ \0]+)'
+IRC_RESP_PARAMS = r'((?: (?:[^\0 \n]*))*?)?(?: :([^\0\n]*))?'
+
+IRC_RESP = '^{}(?:(?:{})|(?:{})){}$'.format(IRC_RESP_PREFIX, IRC_RESP_NUMERIC, IRC_RESP_COMMAND, IRC_RESP_PARAMS)
+IRC_RESP = re.compile(IRC_RESP)
+
+
+
+IRCResponse = namedtuple('IRCResponse', (
+    'server',
+    'is_numeric',
+    'kind',
+    'params'
+))
+
+IRCParams = namedtuple('IRCParams', (
+    'args',
+    'data'
+))
+
+def irc_parse_response(resp: str) -> IRCResponse:
+    """Parses an IRC server response, according to RFC 1459.
+
+        >>> irc_parse_response(':adams.freenode.net 404 :Not Found')
+        IRCResponse(server='adams.freenode.net', is_numeric=True, kind='404', params=IRCParams(args=[''], data='Not Found'))
+
+        >>> irc_parse_response(':adams.freenode.net IS okay :a Good Word')
+        IRCResponse(server='adams.freenode.net', is_numeric=False, kind='IS', params=IRCParams(args=['okay'], data='a Good Word'))
+    
+    Arguments:
+        resp {str} -- The IRC response to parse.
+    
+    Returns:
+        IRCResponse -- The parsed representation.
+    """
+
+    resp = IRC_RESP.match(resp)
+
+    if not resp:
+        return None
+
+    if resp.group(2):
+        is_numeric = True
+        kind = resp.group(2)
+    
+    else:
+        is_numeric = False
+        kind = resp.group(3)
+
+    args = resp.group(4).strip().split(' ')
+    data = resp.group(5)
+
+    params = IRCParams(args, data)
+    return IRCResponse(resp.group(1), is_numeric, kind, params)
 
 
 class IRCConnection(Backend):
     def __init__(self, host, port=6667, ssl=None):
         """Sets up an IRC connection that can be used as
         a triarc backend.
+
+            >>> conn = IRCConnection('abcd')
+            >>> conn._heat
+            0
+            >>> print(conn.ssl_context)
+            None
+
+            >>> import ssl
+            >>> conn = IRCConnection('abcd', ssl=ssl.create_default_context())
+            >>> conn.ssl_context is None
+            False
         
         Arguments:
             host {str} -- The host of the IRC server.
@@ -23,9 +98,185 @@ class IRCConnection(Backend):
         self.host = host
         self.port = port
         self.ssl_context = ssl
+        self.connection = None
+
+        self._out_queue = queue.Queue()
+        self._heat = 0
+
+        self._running = False
+        self._stopping = False
+
+    def running(self):
+        """Returns whether this IRC connection is still up and running.
+
+            >>> conn = IRCConnection('this.does.not.exist')
+            >>> conn.running()
+            False
+        
+        Returns:
+            bool -- Self-explanatory.
+        """
+
+        return self._running
+
+    async def _cooldown(self):
+        while self.running():
+            self._heat = max(self._heat - 1, 0)
+
+            await trio.sleep(1)
+
+    def _send(self, line):
+        self._out_queue.put((line, None))
+
+    async def _send_and_wait(self, line):
+        waiting = [True]
+
+        async def post_wait():
+            waiting[0] = False
+
+        self._out_queue.put((line, post_wait))
+
+        while waiting[0]:
+            await trio.sleep(0.01)
+
+    async def _sender(self):
+        while self.running():
+            while not self._out_queue.empty():
+                self._heat += 1
+
+                if self._heat > 5:
+                    break
+
+                item, on_send = self._out_queue.get()
+
+                await self._send(item)
+
+                if on_send:
+                    await on_send()
+
+            if self._heat > 5:
+                while self._heat:
+                    await trio.sleep(0.2)
+
+            else:
+                await trio.sleep(0.1)
+
+    async def _send(self, item):
+        await self.connection.send_all(str(item).encode('utf-8') + b'\r\n')
+
+    async def _receive(self, line):
+        """
+        This function is called asynchronously everytime
+        the IRC backend receives a response from the
+        remote host (server).
+
+            >>> import trio
+            >>> conn = IRCConnection('i.have.no.mouth.and.i.must.scream')
+            >>> @conn.listen('_NUMERIC')
+            ... def print_received(msg):
+            ...     print(msg)
+            >>> async def print_a_test():
+            ...     print(await conn._receive(':skynet.ai 404 DEATH :AAAAAAAAA'))
+            >>> trio.run(print_a_test)
+            IRCResponse(server='skynet.ai', is_numeric=True, kind='404', params=IRCParams(args=['DEATH'], data='AAAAAAAAA'))
+            True
+
+        Arguments:
+            line {str} -- A single line, after being extracted from received data, and stripped of its trailing CRLF.
+        """
+
+        response = irc_parse_response(line)
+
+        if not response:
+            return False
+
+        if response.is_numeric:
+            received_kind = '_NUMERIC'
+
+        else:
+            received_kind = response.kind
+
+        await self.receive_message(received_kind, response)
+        return True
+
+    async def _receiver(self):
+        while self.running():
+            buffer = ''
+
+            try:
+                async for data in self.connection:
+                    lines = re.split(IRC_SOFT_NEWLINE, buffer + data.decode('utf-8'))
+                    buffer = lines.pop()
+
+                    for l in lines:
+                        await self._receive(l)
+
+                    await trio.sleep(0.01)
+
+            except trio.BrokenResourceError:
+                self.stop()
 
     async def start(self):
-        self.connection = trio.open_tcp_stream(host, port)
+        """Starts the IRC connection.
+
+            >>> import trio
+            >>> PORT = 51523
+            >>> async def mock_irc_server(client):
+            ...     buffer = ""
+            ...     try:
+            ...         async for data in client:
+            ...             lines = re.split(IRC_SOFT_NEWLINE, buffer + data.decode('utf-8'))
+            ...             buffer = lines.pop()
+            ...             for l in lines:
+            ...                 if l == '::STOP!':
+            ...                     raise trio.Cancelled
+            ...                 else:
+            ...                     await client.send_all(b':mock.server ECHO :' + l.encode('utf-8') + b'\\r\\n')
+            ...     except trio.BrokenResourceError:
+            ...         return
+            >>> async def test_irc_server():
+            ...     await trio.serve_tcp(mock_irc_server, PORT)
+            >>> async def test_irc_client():
+            ...     client = IRCConnection('127.0.0.1', PORT)
+            ...     @client.listen('ECHO')
+            ...     async def on_echo(msg):
+            ...         print(msg.kind, ':' + msg.data)
+            ...     async def do_client_things():
+            ...         await trio.sleep(0.1)
+            ...         await client._send_and_wait(b'AAA')
+            ...         await client._send_and_wait(b'::STOP!')
+            ...         await client.stop()
+            ...     async with trio.open_nursery() as nursery:
+            ...         nursery.start_soon(client.start)
+            ...         nursery.start_soon(do_client_things)
+            >>> async def run_test():
+            ...     async with trio.open_nursery() as nursery:
+            ...         nursery.start_soon(test_irc_server)
+            ...         nursery.start_soon(test_irc_client)
+            >>> trio.run(run_test)
+            ECHO :AAA
+        """
+
+        connection = await trio.open_tcp_stream(self.host, self.port)
         
-        if ssl:
-            self.connection = trio.SSLStream(self.connection, ssl)
+        if self.ssl_context:
+            connection = trio.SSLStream(connection, self.ssl_context)
+
+        self.connection = connection
+        self._running = True
+
+        async with trio.open_nursery() as nursery:
+            nursery.start_soon(self._cooldown)
+            nursery.start_soon(self._sender)
+
+        await connection.aclose()
+
+        self._running = False
+
+    async def stop(self):
+        self._stopping = True
+
+        while self.running():
+            await trio.sleep(0.01)
+
+        self._stopping = False
