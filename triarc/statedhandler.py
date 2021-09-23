bbcbfbfac4768d6b9531f64b4edf37cd8235be01
Gustavo6046 <rehermann6046@gmail.com>
2021-09-23 01:42:49 -0300

    Add state enter/exit callbacks

--- triarc/statedhandler.py
+++ triarc/statedhandler.py
@@ -8,21 +8,34 @@ import attr
 import trio  # type: ignore
 
 
-class HandlingState(typing.Protocol):
+@attr.s
+class HandlingState:
     """Class for a state of handling."""
 
     id: str
+
+    async def enter(self, machine: HandlerStateMachine):
+        """Called when entering this state."""
+        pass
+
+    async def exit(self, machine: HandlerStateMachine):
+        """Called when exiting this state."""
+        pass
 
     async def handle(
         self, machine: HandlerStateMachine, data: typing.Any
     ) -> typing.Optional[str]:
         """Handle a command, and optionally return the next state's ID."""
-        ...
+        pass
 
 
 @attr.s(auto_attribs=True)
 class HandlerStateMachine(typing.Protocol):
     """Class for a state machine that can handle asynchronously, with states."""
+
+    def is_on_state(self) -> bool:
+        """Returns True if and only if there is a non-null current state."""
+        ...
 
     def get_state(self) -> typing.Optional[HandlingState]:
         """Get the current state object."""
@@ -52,7 +65,7 @@ class HandlerStateMachine(typing.Protocol):
         self.add_state(handler.id, handler)
         return False
 
-    def next_state(self, state: str) -> bool:
+    async def next_state(self, state: str) -> bool:
         """Try to switch to the next state.
 
         Returns True if and only if the state exists and was switched to."""
@@ -60,7 +73,14 @@ class HandlerStateMachine(typing.Protocol):
         if not self.has_state(state):
             return False
 
+        if self.is_on_state():
+            await self.get_state().exit(self)
+
         self.set_state_name(state)
+
+        if self.is_on_state():
+            await self.get_state().enter(self)
+
         return True
 
     async def close(self):
